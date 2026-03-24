5134b040ea2630463ca3c0ac4208fcb08faa7099
aichaellyadri60-debug <aichaelyadri60@gmail.com>
2026-03-24 11:59:25 +0100

    route de crud des chambres

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,10 @@ use App\Http\Controllers\AuthController;
 Route::get('/', function () {
     return view('home');
 })->name('homepage');
+Route::get('/rooms/create' ,[RoomController::class ,'create'])->name('createRoom');
+Route::get('/rooms', [RoomController::class, 'index'])->name('Room.index');
+Route::post('/rooms' ,[RoomController::class ,'store'])->name('storeRoom');
+Route::get('rooms/{room}/edit' ,[RoomController::class ,'edit'])->name('editRoom');
+Route::put('rooms/{room}' ,[RoomController::class ,'update'])->name('updateRoom');
+Route::delete('rooms/{room}' ,[RoomController::class ,'destroy'])->name('destroyRoom');
+
